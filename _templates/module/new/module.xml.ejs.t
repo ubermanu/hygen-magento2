2a3f56ed99e7e5748e14996bc47ba56e8b7d6f16
ubermanu <manu@paca.happy-dev.fr>
2021-10-28 15:30:47 +0200

    Remove deps and version for the initial creation

--- _templates/module/new/module.xml.ejs.t
+++ _templates/module/new/module.xml.ejs.t
@@ -4,8 +4,4 @@ inject: true
 before: </config>
 skip_if: <module name="<%= name %>".*>
 ---
-<% if (deps.length) { %>    <module name="<%= name %>" setup_version="<%= version %>">
-        <sequence><% for (var x of deps.split(',')) { %>
-            <module name="<%= x %>"/><% } %>
-        </sequence>
-    </module><% } else { %>    <module name="<%= name %>" setup_version="<%= version %>"/><% } %>
+    <module name="<%= name %>"/>
--- _templates/module/new/prompt.js
+++ _templates/module/new/prompt.js
@@ -3,15 +3,5 @@ module.exports = [
         type: 'input',
         name: 'name',
         message: "What's your module name? (ex: Vendor_Package)"
-    },
-    {
-        type: 'input',
-        name: 'version',
-        message: "What's your module version? (ex: 1.0.0)"
-    },
-    {
-        type: 'input',
-        name: 'deps',
-        message: "A comma separated list of dependencies (ex: Magento_Catalog,Magento_Sales)"
     }
 ]
